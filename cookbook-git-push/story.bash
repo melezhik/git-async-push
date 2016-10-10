55e761441b25e62797256ab62324da4581c6676d
Alexey Melezhik <melezhik@gmail.com>
2016-10-10 16:28:26 +0300

    up

--- cookbook-git-push/story.bash
+++ cookbook-git-push/story.bash
@@ -1,8 +1,16 @@
 local_dir=$(config local_dir)
 export url=$(config url)
+
+
 cd $local_dir
+
+echo url: $url
+echo local_dir: $local_dir
+echo pwd: `pwd`
+
+
 find -maxdepth 2 -mindepth 2 -name metadata.rb \
 -execdir perl6 -e 'say "cd " ~ $*CWD.basename 
 ~ " ; (git  push --set-upstream %*ENV<url>/" 
 ~ $*CWD.basename ~ " master  -q  || echo " 
-~ $*CWD.basename  ~ " --- failed ) &  cd ../ " ' \; | bash  && echo ok
+~ $*CWD.basename  ~ " --- failed ) &  cd ../ " ' \; | bash  && echo cookbook-git-push-done
