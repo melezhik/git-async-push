261e8b97448a6223e842c7a2917d485f0f366530
Alexey Melezhik <melezhik@gmail.com>
2016-10-12 11:03:19 +0300

    git pull and project

--- story.bash
+++ story.bash
@@ -1,5 +1,6 @@
 local_dir=$(config local_dir)
 export url=$(config url)
+export project=$(config project)
 
 echo url: $url
 echo local_dir: $local_dir
@@ -7,6 +8,16 @@ echo local_dir: $local_dir
 cd $local_dir
 
 find $local_dir -maxdepth 1 -mindepth 1 -type d -execdir perl -MFile::Basename \
--e 'my $p = basename(@ARGV[0]); my $wd = $ARGV[1]; 
-print "cd $wd/$p && ( git push --set-upstream $ENV{url}/$p master -q ",
+-e '
+my $p = basename(@ARGV[0]); 
+my $wd = $ARGV[1]; 
+my $project = $ENV{project};
+my $url = $ENV{url};
+
+if ($project){
+  exit unless $p eq $project;
+}
+
+print "cd $wd/$p && ( git pull $url/$p.git && \\
+git push --set-upstream $url/$p.git master -q ",
 " || echo $p -- failed ) & \n"' {} $local_dir \; | bash && echo git-async-push-done
